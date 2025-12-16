25b8e089d2b0c71f3d20d5338d11bf6308ea4e6e
Mirza-Usman-247 <usemanbaig2008@gmail.com>
2025-12-17 04:24:13 +0500

    final deploy 4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://physical-ai-and-humanoid-robotics-t-psi.vercel.app/', // Vercel URL
+  url: 'https://physical-ai-and-humanoid-robotics-t-psi.vercel.app', // Vercel URL
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: '/', // Root path for Vercel deployment
 
